2c9acfe4815757e7ebca570a64569a5b920e7fd2
Aniol <53788631+Aniol0012@users.noreply.github.com>
2022-12-13 13:21:23 +0100

    Add files via upload

--- src/prac2_4.sh
+++ src/prac2_4.sh
@@ -9,14 +9,12 @@
 #------------------------------------------------------------------
 
 # Definim una funció que agafa el nom del fitxer com a argument
-
 tipFitxer() {
 	# Guardem el nom del fitxer en una variable
 	local filename=$1
 
 	# Usem la comanda "file" per a obtenir el tipus de fitxer i el format
 	local file_output=$(file $filename)
-
 
 	# Guardem l'estat de la comanda "file" prèviament realitzada
 	local exit_status=$?
@@ -47,12 +45,10 @@ if [ $# -ne 0 ]
 		fi
 	done
 else # En el cas que no passem cap paràmetre
-	ruta=$(pwd)
-	echo "Llistat de fitxers del directori [$ruta]"
+	echo "Llistat de fitxers del directori [$(pwd)]:"
 	echo ""
 
 	# Creem el llistat de fitxers del directori en cas que sigui possible
-	
 	for variable_param in *
 		do
 		if [ -d "$variable_param" ] # En cas que existeixi i sigui un directori
